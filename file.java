ae35d88d1d06292e4cf1051e67708250ea8558fb
MounikaNammina <namminamounika@gmail.com>
2024-06-13 18:35:03 +0530

    Create file.java

---
+++ file.java
@@ -0,0 +1,8 @@
+```java
+public class VulnerableCode {
+    public static void main(String[] args) {
+        String[] names = {"Alice", "Bob", "Charlie"};
+        int index = 3;
+        System.out.println(names[index]);
+    }
+}
